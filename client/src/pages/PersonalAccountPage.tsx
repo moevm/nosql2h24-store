ac95f928fdd0bccd6f4342874543664a7fbb6d40
Test User <polina.koroleva05@mail.ru>
2024-10-22 18:26:49 +0300

    add routing from personal account page

--- client/src/pages/PersonalAccountPage.tsx
+++ client/src/pages/PersonalAccountPage.tsx
@@ -1,19 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import MyCellsPage from './MyCellsPage';
+import BookCellPage from './BookCellPage';
+import HelpPage from './HelpPage';
 
 
 export default function PersonalAccount() {
     return (<>
         PersonalPage
-        <BrowserRouter>
-            <Link to="/myCells">Мои ячейки</Link>
-            <Link to="/bookCell">Арендовать ячейку</Link>
-            <Link to="/help">Темы</Link>
-            <Routes>
-                <Route path="/myCells" element={<MyCellsPage/>}></Route>
-                <Route path="/bookCell" element={<BookCellPage/>}></Route>
-                <Route exact path="/help" element={<HelpPage />}></Route>
-            </Routes>
-        </BrowserRouter>
+        <Link to="/myCells">Мои ячейки</Link>
+        <Link to="/bookCell">Арендовать ячейку</Link>
+        <Link to="/help">Темы</Link>
     </>)
 }
