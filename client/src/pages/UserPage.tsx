77cf3b842c1bd8cba76b3a771fe8eb1f79cafdb2
Test User <polina.koroleva05@mail.ru>
2024-11-02 13:58:02 +0300

    Create UserPage.tsx

---
+++ client/src/pages/UserPage.tsx
@@ -0,0 +1,15 @@
+import { User } from "../types/types"
+import { useLocation } from "react-router-dom"
+
+export default function UserPage() {
+    const {state} = useLocation();
+    return (<div>
+        <h2>{state.NameSurnamePatronymic}</h2>
+        <div>
+            <p>Пользователь Ячейка.ру с {state.date}</p>
+            <p>Роль: {state.role}</p>
+            <p>Арендованные ячейки: {state.rentedCells.join(',')}</p>
+            <p>Задолженность: {state.indebtedness > 0.00 ? state.indebtedness : "нет"}</p>
+        </div>
+    </div>)
+}
