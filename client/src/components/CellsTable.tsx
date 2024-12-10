9b8b0470dcfffa589078d67ef15979943b8b6e59
Novak Polina <polianovak57@gmail.com>
2024-12-11 01:16:57 +0300

    Cells Table Frontend changes

--- client/src/components/CellsTable.tsx
+++ client/src/components/CellsTable.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import Payment from "../pages/PaymentPage";
 import { useState } from "react";
 import { Cell } from "../serviceFiles/types";
@@ -6,83 +6,98 @@ import Table from "react-bootstrap/Table";
 import { Button } from "react-bootstrap";
 import axios from "axios";
 import { POST_SERVICE_CELL_URL } from "../serviceFiles/constants";
+import "./CellsTable.css";
 
-export default function CellsTable(props: { isForRent: boolean, isForAdmin: boolean, cells: Cell[] }) {
-    let navigate = useNavigate();
-    const [showPayment, setShowPayment] = useState(false);
-    const [cellForRent, setCellForRent] = useState(props.cells[0]);
-    // let cellForRent: Cell|null = null;
-    function handleRent(cell: Cell) {
-        navigate("/paymentCell", { state: cell })
-        // setCellForRent(cell);
-        // console.log(cellForRent);
-        // setShowPayment(!showPayment);
-        // console.log("again", cellForRent);
-    }
-    function handleService(cell: Cell) {
-        let key = sessionStorage.getItem("key");
-        console.log("обслуживание ячейки с данными:", { userKey: key, cell });
-        axios.post(POST_SERVICE_CELL_URL, { userKey: key, cell }).catch (error => {
-        console.error('Ошибка при обслуживании Ячейки', error);
+export default function CellsTable(props: {
+  isForRent: boolean;
+  isForAdmin: boolean;
+  cells: Cell[];
+}) {
+  let navigate = useNavigate();
+  const [showPayment, setShowPayment] = useState(false);
+  const [cellForRent, setCellForRent] = useState(props.cells[0]);
+  // let cellForRent: Cell|null = null;
+  function handleRent(cell: Cell) {
+    navigate("/paymentCell", { state: cell });
+    // setCellForRent(cell);
+    // console.log(cellForRent);
+    // setShowPayment(!showPayment);
+    // console.log("again", cellForRent);
+  }
+  function handleService(cell: Cell) {
+    let key = sessionStorage.getItem("key");
+    console.log("обслуживание ячейки с данными:", { userKey: key, cell });
+    axios.post(POST_SERVICE_CELL_URL, { userKey: key, cell }).catch((error) => {
+      console.error("Ошибка при обслуживании Ячейки", error);
     });
-}
-function CloseRent() {
+  }
+  function CloseRent() {
     setShowPayment(!showPayment);
-}
+  }
 
-function handleCellClick(cell: Cell) {
-    navigate("/cell", { state: cell })
-}
+  function handleCellClick(cell: Cell) {
+    navigate("/cell", { state: cell });
+  }
 
-const listCells = props.cells.map((cell: Cell, index) =>
-    <tr key={cell._key}>
-        <td>
-            {cell._key}
+  const listCells = props.cells.map((cell: Cell, index) => (
+    <tr className="listCellsTable" key={cell._key}>
+      <td>{cell._key}</td>
+      <td>{cell.cellNum}</td>
+      <td>{cell.isFree ? "Свободна" : "Занята"}</td>
+      <td>{cell.endOfRent}</td>
+      <td>{cell.warehouseKey}</td>
+      {props.isForRent && (
+        <td className="listCellsTd">
+          <button
+            type="button"
+            className="button"
+            onClick={() => handleRent(cell)}
+          >
+            Арендовать
+          </button>
         </td>
+      )}
+      {props.isForAdmin && (
+        <td> {cell.needService ? "Требует" : "Не требует"}</td>
+      )}
+      {props.isForAdmin && (
         <td>
-            {cell.cellNum}
+          <button
+            type="button"
+            className="button"
+            onClick={() => handleService(cell)}
+          >
+            Обслужить
+          </button>
         </td>
-        <td>
-            {cell.isFree ? "Свободна" : "Занята"}
-        </td>
-        <td>
-            {cell.endOfRent}
-        </td>
-        <td>
-            {cell.warehouseKey}
-        </td>
-        {props.isForRent && <td> <Button type="button" className="btn" onClick={() => handleRent(cell)}> Арендовать </Button></td>}
-        {props.isForAdmin && <td> {cell.needService ? "Требует" : "Не требует"}</td>}
-        {props.isForAdmin && <td> <Button type="button" className="btn" onClick={() => handleService(cell)}> Обслужить </Button> </td>}
-        <td> <Button type="button" className="btn" onClick={() => handleCellClick(cell)}> Подробнее </Button></td>
+      )}
+      <td>
+        <button
+          type="button"
+          className="button"
+          onClick={() => handleCellClick(cell)}
+        >
+          Подробнее
+        </button>
+      </td>
     </tr>
-)
-return (<>
-    {/* {showPayment && <Payment cell={cellForRent} handleClick={CloseRent} isOpen={showPayment} />} */}
-    <Table striped bordered hover>
+  ));
+  return (
+    <div className="cellsBody">
+      {/* {showPayment && <Payment cell={cellForRent} handleClick={CloseRent} isOpen={showPayment} />} */}
+      <table className="cellsTable">
         <thead>
-            <tr>
-                <th scope="col">
-                    _key ячейки
-                </th>
-                <th scope="col">
-                    Номер
-                </th>
-                <th scope="col">
-                    Статус
-                </th>
-                <th scope="col">
-                    Дата окончания аренды
-                </th>
-                <th scope="col">
-                    _key склада
-                </th>
-                {props.isForAdmin && <th scope="col"> Тех.обслуживание </th>}
-            </tr>
+          <tr>
+            <th scope="col">_key ячейки</th>
+            <th scope="col">Номер</th>
+            <th scope="col">Статус</th>
+            <th scope="col">Дата окончания аренды</th>
+            <th scope="col">_key склада</th>
+            {props.isForAdmin && <th scope="col"> Тех.обслуживание </th>}
+          </tr>
         </thead>
-        <tbody>
-            {listCells}
-        </tbody>
-    </Table>
-</>)
+        <tbody className="cellsTableBody">{listCells}</tbody>
+      </table>
+    </div>
+  );
 }
--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -1,63 +1,73 @@
 import { useState } from "react";
 import { Button, Form, InputGroup } from "react-bootstrap";
 
-export default function Filter(props: { handleSend: any, obj: any }) {
-    const [state, setState] = useState(props.obj);
+export default function Filter(props: { handleSend: any; obj: any }) {
+  const [state, setState] = useState(props.obj);
 
-    function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const formDataObj = Object.fromEntries(formData.entries());
-        props.handleSend(formDataObj);
-    }
-    const filters = []
-    for (let key in props.obj) {
-        filters.push(
-            <>
-                <InputGroup.Text id="basic-addon1">{props.obj[key].name}</InputGroup.Text>
-                {props.obj[key].type == "b" && <InputGroup.Checkbox name={key} />}
-                {props.obj[key].type == "s" && <Form.Control
-                    placeholder={props.obj[key].name} name={key}
-                />}
-                {
-                    props.obj[key].type == "d" && <Form.Control type="datet"
-                        placeholder={props.obj[key].name} name={key}
-                    />}
-                {
-                    props.obj[key].type == "dt" && <Form.Control type="datetime-local"
-                        placeholder={props.obj[key].name} name={key}
-                    />}
-                {
-                    props.obj[key].type == "n" && <Form.Control type="number"
-                        placeholder={props.obj[key].name} name={key}
-                    />}
-                {
-                    props.obj[key].type == "nf" && <Form.Control
-                        type="number"
-                        step={0.1}
-                        placeholder={props.obj[key].name} name={key}
-                    />
-                }
-                {
-                    props.obj[key].type == "l" && <Form.Control
-                        placeholder={props.obj[key].name} name={key} pattern="[0-9,]+"
-                    />
-                }
-
-            </>
-        )
-
-    }
-    return (<>
-        <Form onSubmit={handleSubmit}>
-            <InputGroup className="mb-3">
-                {filters}
-            </InputGroup>
-            <Button type="submit">Поиск</Button>
-        </Form>
-
-    </>)
-
+  function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formDataObj = Object.fromEntries(formData.entries());
+    props.handleSend(formDataObj);
+  }
+  const filters = [];
+  for (let key in props.obj) {
+    filters.push(
+      <>
+        <InputGroup.Text id="basic-addon1">
+          {props.obj[key].name}
+        </InputGroup.Text>
+        {props.obj[key].type == "b" && <InputGroup.Checkbox name={key} />}
+        {props.obj[key].type == "s" && (
+          <Form.Control placeholder={props.obj[key].name} name={key} />
+        )}
+        {props.obj[key].type == "d" && (
+          <Form.Control
+            type="datet"
+            placeholder={props.obj[key].name}
+            name={key}
+          />
+        )}
+        {props.obj[key].type == "dt" && (
+          <Form.Control
+            type="datetime-local"
+            placeholder={props.obj[key].name}
+            name={key}
+          />
+        )}
+        {props.obj[key].type == "n" && (
+          <Form.Control
+            type="number"
+            placeholder={props.obj[key].name}
+            name={key}
+          />
+        )}
+        {props.obj[key].type == "nf" && (
+          <Form.Control
+            type="number"
+            step={0.1}
+            placeholder={props.obj[key].name}
+            name={key}
+          />
+        )}
+        {props.obj[key].type == "l" && (
+          <Form.Control
+            placeholder={props.obj[key].name}
+            name={key}
+            pattern="[0-9,]+"
+          />
+        )}
+      </>
+    );
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <InputGroup className="mb-3">{filters}</InputGroup>
+        <Button type="submit">Поиск</Button>
+      </form>
+    </>
+  );
 }
 
 // {
