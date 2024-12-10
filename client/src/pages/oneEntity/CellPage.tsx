2631431dd109531754921d64bc368a5257d84ec8
Novak Polina <polianovak57@gmail.com>
2024-12-10 20:56:50 +0300

    Cell Page frontend changes

--- client/src/pages/oneEntity/CellPage.tsx
+++ client/src/pages/oneEntity/CellPage.tsx
@@ -1,18 +1,23 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
+import "../../css/oneEntity/CellPage.css";
 
 export default function CellPage() {
-    const {state} = useLocation();
-    console.log(state)
-    return (<div>
-        <h2>Ячейка id {state._key}</h2>
+  const { state } = useLocation();
+  console.log(state);
+  return (
+    <div className="cellPageContainer">
+      <div className="cellPageBody">
+        <h2>Ячейка id &nbsp; {/*state._key*/}</h2>
         <div>
-            <p>Номер {state.cellNum}</p>
-            <p>Склад {state.warehouseId}</p>
-            <p>Ряд {state.tierNum}</p>
-            <p>Свободна {state.isFree}</p>
-            <p>Конец аренды {state.endOfRent}</p>
-            <p>Тариф {state.tariffPerDay}</p>
-            <p>Размер {state.size}</p>
+          <p>Номер &nbsp; {/*state.cellNum*/}</p>
+          <p>Склад &nbsp; {/*state.warehouseId*/}</p>
+          <p>Ряд &nbsp; {/*state.tierNum*/}</p>
+          <p>Свободна &nbsp; {/*state.isFree*/}</p>
+          <p>Конец аренды &nbsp; {/*state.endOfRent*/}</p>
+          <p>Тариф &nbsp; {/*state.tariffPerDay*/}</p>
+          <p>Размер &nbsp; {/*state.size*/}</p>
         </div>
-    </div>)
+      </div>
+    </div>
+  );
 }
