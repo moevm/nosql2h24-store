079d1dd72890bdb0133b83b102830e321e94d135
Novak Polina <polianovak57@gmail.com>
2024-12-10 23:21:12 +0300

    User Page Frontend changes

--- client/src/pages/oneEntity/UserPage.tsx
+++ client/src/pages/oneEntity/UserPage.tsx
@@ -1,15 +1,23 @@
-import { User } from "../../serviceFiles/types"
-import { useLocation } from "react-router-dom"
+import { User } from "../../serviceFiles/types";
+import { useLocation } from "react-router-dom";
+import "../../css/oneEntity/UserPage.css";
 
 export default function UserPage() {
-    const {state} = useLocation();
-    return (<div>
-        <h2>{state.nameSurnamePatronymic}</h2>
+  const { state } = useLocation();
+  return (
+    <div className="userPageContainer">
+      <div className="userPageBody">
+        <h2>{/*state.nameSurnamePatronymic*/}</h2>
         <div>
-            <p>Пользователь Ячейка.ру с {state.regDate}</p>
-            <p>Роль: {state.role}</p>
-            {/* <p>Арендованные ячейки: {state.rentedCells.join(',')}</p> */}
-            <p>Задолженность: {state.indebtedness > 0.00 ? state.indebtedness : "нет"}</p>
+          <p>Пользователь Ячейка.ру: &nbsp; {/*state.regDate*/}</p>
+          <p>Роль: &nbsp; {/*state.role*/}</p>
+          {/* <p>Арендованные ячейки: &nbsp; {state.rentedCells.join(',')}</p> */}
+          <p>
+            Задолженность: &nbsp;
+            {/*state.indebtedness > 0.00 ? state.indebtedness : "нет"*/}
+          </p>
         </div>
-    </div>)
+      </div>
+    </div>
+  );
 }
