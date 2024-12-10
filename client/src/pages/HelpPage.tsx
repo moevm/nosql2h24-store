b20dbb8762638b3b33737551bc30f58c09ba949d
Novak Polina <polianovak57@gmail.com>
2024-12-10 19:39:30 +0300

    Help Page frontend changes

--- client/src/pages/HelpPage.tsx
+++ client/src/pages/HelpPage.tsx
@@ -4,43 +4,48 @@ export default function HelpPage() {
   return (
     <div className="helpPageContainer">
       <div className="helpPageBody">
-        <h1 className="helpPageContactTitle">Служба поддержки</h1>
-        <p className="helpPageContactDescription">
-          Мы всегда рады вопросам и фидбеку, поэтому предлагаем контакты для
-          связи!
-        </p>
+        <div className="helpPageForm">
+          <h1 className="helpPageContactTitle">Служба поддержки</h1>
+          <p className="helpPageContactDescription">
+            Мы всегда рады вопросам и фидбеку, поэтому предлагаем контакты для
+            связи!
+          </p>
+          <data className="formItem">
+            <img
+              className="dataImage"
+              width="30"
+              height="30"
+              src="./icons/mail-icon.svg"
+              alt="Email"
+              loading="lazy"
+            />
+            <span className="dataTitle">Email: yacheika.ru@company.co</span>
+          </data>
 
-        <form className="helpPageForm">
-          <label className="visually-hidden" htmlFor="email">
-            Почта
-          </label>
-          <input
-            className="dataInput"
-            id="email"
-            type="email"
-            placeholder="Email: yacheika.ru@company.co"
-          ></input>
+          <data className="formItem">
+            <img
+              className="dataImage"
+              width="30"
+              height="30"
+              src="./icons/phone2-icon.svg"
+              alt="Email"
+              loading="lazy"
+            />
+            <span className="dataTitle">Phone: (123) 456-7890</span>
+          </data>
+        </div>
 
-          <label className="visually-hidden" htmlFor="phone">
-            Телефон
-          </label>
-          <input
-            className="dataInput"
-            id="phone"
-            type="phone"
-            placeholder="Phone: (123) 456-7890"
-          ></input>
-        </form>
+        <div className="helpPageRequest">
+          <p className="helpPageFormRequest">
+            Оставьте свое сообщение, и оператор ответит вам в ближайшее время.
+          </p>
+          <textarea
+            className="helpPageFormTextarea"
+            placeholder="Оставьте свое сообщение здесь..."
+          ></textarea>
 
-        <p className="helpPageFormRequest">
-          Оставьте свое сообщение, и оператор ответит вам в ближайшее время.
-        </p>
-        <textarea
-          className="helpPageFormTextarea"
-          placeholder="Оставьте свое сообщение здесь..."
-        ></textarea>
-
-        <button className="requestButton button">Отправить сообщение</button>
+          <button className="requestButton button">Отправить сообщение</button>
+        </div>
       </div>
     </div>
   );
