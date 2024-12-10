d962473a37e6a336f19e2b2a70c6a0a011ce3549
Novak Polina <polianovak57@gmail.com>
2024-12-10 20:58:31 +0300

    Cell Page frontend changes 2

--- client/src/pages/oneEntity/CellPage.tsx
+++ client/src/pages/oneEntity/CellPage.tsx
@@ -7,15 +7,15 @@ export default function CellPage() {
   return (
     <div className="cellPageContainer">
       <div className="cellPageBody">
-        <h2>Ячейка id &nbsp; {/*state._key*/}</h2>
+        <h2>Ячейка id &nbsp; {state._key}</h2>
         <div>
-          <p>Номер &nbsp; {/*state.cellNum*/}</p>
-          <p>Склад &nbsp; {/*state.warehouseId*/}</p>
-          <p>Ряд &nbsp; {/*state.tierNum*/}</p>
-          <p>Свободна &nbsp; {/*state.isFree*/}</p>
-          <p>Конец аренды &nbsp; {/*state.endOfRent*/}</p>
-          <p>Тариф &nbsp; {/*state.tariffPerDay*/}</p>
-          <p>Размер &nbsp; {/*state.size*/}</p>
+          <p>Номер &nbsp; {state.cellNum}</p>
+          <p>Склад &nbsp; {state.warehouseId}</p>
+          <p>Ряд &nbsp; {state.tierNum}</p>
+          <p>Свободна &nbsp; {state.isFree}</p>
+          <p>Конец аренды &nbsp; {state.endOfRent}</p>
+          <p>Тариф &nbsp; {state.tariffPerDay}</p>
+          <p>Размер &nbsp; {state.size}</p>
         </div>
       </div>
     </div>
