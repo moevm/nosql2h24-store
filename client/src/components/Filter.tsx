5d1dd966aaf5c8bba88de6be05d2cfe4599ef367
Test User <polina.koroleva05@mail.ru>
2024-12-11 17:43:39 +0300

    filters fix

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -1,79 +1,71 @@
 import { useState } from "react";
-import { Form, InputGroup } from "react-bootstrap";
+import { Button, Form, InputGroup } from "react-bootstrap";
 import "./Filter.css";
 
-export default function Filter(props: { handleSend: any; obj: any }) {
-  const [state, setState] = useState(props.obj);
+export default function Filter(props: { handleSend: any, obj: any }) {
+    const [state, setState] = useState(props.obj);
 
-  function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formDataObj = Object.fromEntries(formData.entries());
-    props.handleSend(formDataObj);
-  }
-  const filters = [];
-  for (let key in props.obj) {
-    filters.push(
-      <>
-        <InputGroup.Text id="basic-addon1">
-          {props.obj[key].name}
-        </InputGroup.Text>
-        {props.obj[key].type == "b" && <InputGroup.Checkbox name={key} />}
-        {props.obj[key].type == "s" && (
-          <Form.Control placeholder={props.obj[key].name} name={key} />
-        )}
-        {props.obj[key].type == "d" && (
-          <Form.Control
-            type="datet"
-            placeholder={props.obj[key].name}
-            name={key}
-          />
-        )}
-        {props.obj[key].type == "dt" && (
-          <Form.Control
-            type="datetime-local"
-            placeholder={props.obj[key].name}
-            name={key}
-          />
-        )}
-        {props.obj[key].type == "n" && (
-          <Form.Control
-            type="number"
-            placeholder={props.obj[key].name}
-            name={key}
-          />
-        )}
-        {props.obj[key].type == "nf" && (
-          <Form.Control
-            type="number"
-            step={0.1}
-            placeholder={props.obj[key].name}
-            name={key}
-          />
-        )}
-        {props.obj[key].type == "l" && (
-          <Form.Control
-            placeholder={props.obj[key].name}
-            name={key}
-            pattern="[0-9,]+"
-          />
-        )}
-      </>
-    );
-  }
-  return (
-    <div className="filterContainer">
-      <form className="filterContainer" onSubmit={handleSubmit}>
-        <InputGroup className="filterInput">{filters}</InputGroup>
-        <button className="button" type="submit">
-          Поиск
-        </button>
-      </form>
-    </div>
-  );
+    function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formDataObj = Object.fromEntries(formData.entries());
+        props.handleSend(formDataObj);
+    }
+    const filters = []
+    for (let key in props.obj) {
+        filters.push(
+            <>
+                <InputGroup.Text id="basic-addon1">{props.obj[key].name}</InputGroup.Text>
+                {props.obj[key].type == "b" && <InputGroup.Checkbox name={key} />}
+                {props.obj[key].type == "s" && <div><Form.Control
+                    placeholder={props.obj[key].name} name={key}
+                /></div>}
+                {
+                    props.obj[key].type == "d" && <div><Form.Control type="date"
+                        placeholder={props.obj[key].name  + ' от'} name={'start'+key}
+                    /><Form.Control type="date"
+                    placeholder={props.obj[key].name  + ' до'} name={'end'+key}
+                /></div>}
+                {
+                    props.obj[key].type == "dt" && <div><Form.Control type="datetime-local"
+                        placeholder={props.obj[key].name + ' от'} name={'start'+key}
+                    /><Form.Control type="datetime-local"
+                    placeholder={props.obj[key].name + ' до'} name={'end'+key}
+                /></div>}
+                {
+                    props.obj[key].type == "n" && <div><Form.Control type="number"
+                        placeholder={props.obj[key].name + ' от'} name={'start'+key}
+                    /><Form.Control type="number"
+                    placeholder={props.obj[key].name + ' до'} name={'end'+key}
+                /></div>}
+                {
+                    props.obj[key].type == "nf" && <div><Form.Control
+                        type="number"
+                        step={0.1}
+                        placeholder={props.obj[key].name + ' от'} name={'start'+key}
+                    /><Form.Control
+                    type="number"
+                    step={0.1}
+                    placeholder={props.obj[key].name + ' до'} name={'end'+key}
+                /></div>
+                }
+                {
+                    props.obj[key].type == "l" && <Form.Control
+                        placeholder={props.obj[key].name} name={key} pattern="[0-9,]+"
+                    />
+                }
+
+            </>
+        )
+
+    }
+    return (<>
+        <Form onSubmit={handleSubmit}>
+            <InputGroup className="mb-3">
+                {filters}
+            </InputGroup>
+            <Button type="submit">Поиск</Button>
+        </Form>
+
+    </>)
 }
-
-// {
-//     data: "Дата",
-//     FIO: "Фамилия Имя Отчество"
-// }
